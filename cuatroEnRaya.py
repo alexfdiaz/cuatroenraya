606ae06f85d0f02af02383490c23291e5d295763
Alex Diaz <alexdiaz18012002@gmail.com>
2021-05-19 15:56:14 -0300

    Funcion para obtener el contenido de una fila, funcion que lista el tablero por filas, funcion que lista el tablero por columnas

--- cuatroEnRaya.py
+++ cuatroEnRaya.py
@@ -15,6 +15,34 @@ def contenidoColumna(nro_columna, tablero):
 		columna.append(celda)
 	return columna
 
+def contenidoFila(nro_fila, tablero):
+	return tablero[nro_fila - 1]
+
+def columnas(tablero):
+	listaDeColumnas = [
+	[0, 0, 0, 0, 0, 0],
+	[0, 0, 0, 0, 0, 0],
+	[0, 0, 0, 0, 0, 0],
+	[0, 0, 0, 0, 0, 0],
+	[0, 0, 0, 0, 0, 0],
+	[0, 0, 0, 0, 0, 0],
+	[0, 0, 0, 0, 0, 0]
+	]
+
+	i = 0
+	j = 0
+	for fila in tablero:
+		for celda in fila:
+			listaDeColumnas[j][i] = celda
+			j += 1
+		i += 1
+		j = 0
+	return listaDeColumnas
+
+def filas(tablero):
+	listaDeFilas = tablero
+	return listaDeFilas
+
 def completarTableroEnOrden(secuencia, tablero):
 	for indice, columna in enumerate(secuencia):
 		fichaNumero = 1 + (indice % 2)
@@ -31,7 +59,7 @@ def dibujarTablero(tablero):
 	for fila in tablero:
 		for celda in fila:
 			if celda == 0:
-				print('   ', end='')
+				print(' 0 ', end='')
 			else:
 				print(' %s ' % celda, end='')
 		print('')
@@ -47,8 +75,18 @@ secuencia = [1, 2, 3, 1, 3, 4]
 tablero = []
 if secuenciaValida(secuencia):
 	tablero = completarTableroEnOrden(secuencia, tableroVacio())
-	#dibujarTablero(tablero)
+	dibujarTablero(tablero)
 else:
 	print("Las columnas deberian ir de 1 a 7")
 
-print(contenidoColumna(2, tablero))
+print("Contenido de una columna")
+print(contenidoColumna(1, tablero))
+
+print("Contenido de una fila")
+print(contenidoFila(6, tablero))
+
+print("Listado de columnas")
+print(columnas(tablero))
+
+print("Listado de filas")
+print(filas(tablero))
